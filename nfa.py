3f43d2cdf96e2e684187adec4d049cd231e909b0
Petros Markopoulos <petrosmarkopoulos01@gmail.com>
2021-01-01 09:37:52 -0500

    Complete analyze implementation with helpers

--- nfa.py
+++ nfa.py
@@ -9,6 +9,24 @@ class NFA:
     def __len__(self):
         return len(self._states)
 
+    def analyze(self, message):
+        if not self._isValid():
+            print('This NFA is missing some transitions')
+            return False
+        if not self._validMessage(message):
+            print('The message has to consist of characters from the alphabet')
+            return False
+
+        curr = [(self._states[0], message)]
+        while len(curr) != 0:
+            s, m = curr.pop(0)
+            if len(m) == 0 and s.isFinal():
+                return True
+            curr.extend(map(lambda x: (x, m), self._transitions[(s, '')]))
+            if len(m) > 0:
+                curr.extend(map(lambda x: (x, m[1:]), self._transitions[(s, m[0])]))
+        return False
+
     def getState(self, idx):
         return self._states[idx]
 
@@ -31,3 +49,16 @@ class NFA:
 
     def setTransition(self, src, wth, dst):
         self._transitions[(src,wth)].append(dst)
+
+    def _validMessage(self, msg):
+        res = True
+        for c in msg:
+            res = res and (c in self._alphabet)
+        return res
+
+    def _isValid(self):
+        res = True
+        for state in self._states:
+            for c in self._alphabet:
+                res = res and (c == '' or (len(self._transitions[(state,c)]) > 0))
+        return res
